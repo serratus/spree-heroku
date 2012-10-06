0a911b4656f75f0cf938fda6009af90037c5ef5b
Christoph Oberhofer <ch.oberhofer@gmail.com>
2012-10-06 09:30:29 -0400

    added s3 protocol to serve http

--- app/models/image_decorator.rb
+++ app/models/image_decorator.rb
@@ -7,6 +7,7 @@ Spree::Image.class_eval do
           :access_key_id     => ENV['S3_KEY'],
           :secret_access_key => ENV['S3_SECRET']
         },
+        :s3_protocol => "http",
         :bucket => ENV['S3_BUCKET'],
         :s3_host_alias => ENV['S3_HOST_ALIAS']
       }
