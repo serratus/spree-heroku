df8e567a3b95d152b41f01351244a2a7509d8b90
Christoph Oberhofer <ch.oberhofer@gmail.com>
2012-10-08 23:49:05 +0200

    removed url-alias

--- app/models/image_decorator.rb
+++ app/models/image_decorator.rb
@@ -1,14 +1,13 @@
 Spree::Image.class_eval do
   if Rails.env.production?
-    if ENV['S3_KEY'] && ENV['S3_SECRET'] && (ENV['S3_BUCKET'] || ENV['S3_HOST_ALIAS'])
+    if ENV['S3_KEY'] && ENV['S3_SECRET'] && ENV['S3_BUCKET']
       S3_OPTIONS = {
         :storage => 's3',
         :s3_credentials => {
           :access_key_id     => ENV['S3_KEY'],
           :secret_access_key => ENV['S3_SECRET']
         },
-        :bucket => ENV['S3_BUCKET'],
-        :s3_host_alias => ENV['S3_HOST_ALIAS']
+        :bucket => ENV['S3_BUCKET']
       }
     else
       S3_OPTIONS = {
